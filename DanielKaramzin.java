47b9d92be454b584f42ae829d24894dcf4a5df8f
DanielK3005 <danielkaramzin@gmail.com>
2024-05-12 18:53:48 +0300

    change danielkaramzin java

--- DanielKaramzin.java
+++ DanielKaramzin.java
@@ -2,7 +2,7 @@ public class DanielKaramzin
 {
 	public static void main(String[] args) {
 		for (int i=1;i<=5; i++) {
-			System.out.println("Happy Birthday!");
+			System.out.println("Yom Tov!");
 		}
 	}
 }
